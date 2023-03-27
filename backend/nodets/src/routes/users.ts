d2cf35203411d15983d7b6b56e49a6177d998099
Craig Daniels <craigrdaniels@gmail.com>
2023-03-28 06:38:38 +1000

    add JWT token to user create

---
+++ backend/nodets/src/routes/users.ts
@@ -0,0 +1,53 @@
+import { type Request, type Response, type NextFunction } from 'express'
+import express from 'express'
+import bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+import User from '../models/user.js'
+
+dotenv.config()
+
+const usersRouter = express.Router()
+
+usersRouter.post(
+  '/users/signup',
+  (req: Request, res: Response, next: NextFunction): void => {
+    void (async (): Promise<void> => {
+      try {
+        console.log(req.body.email) // BUG: #2 Returns undefined
+        if (
+          req.body?.email === undefined ||
+          req.body?.email.trim().length < 1
+        ) {
+          // TODO: #1 need to validate it is a real email address?
+          next({ status: 400, message: 'Email is required' })
+          return
+        }
+        if (
+          req.body?.password === undefined ||
+          req.body?.password.trim().length < 1
+        ) {
+          next({ status: 400, message: 'Password is required' })
+          return
+        }
+
+        const user = new User({
+          email: req.body.email,
+          password: bcrypt.hashSync(req.body.password, 10),
+          username: req.body.username.trim() ?? req.body.email.split('@')[0]
+        })
+        await user.save()
+        const token = jwt.sign(
+          { id: user._id, email: user.email },
+          process.env.JWT_SECRET_KEY ?? ''
+        )
+        res.status(200).json({ success: true, message: 'User created', token })
+      } catch (error) {
+        console.log(error)
+        next(error)
+      }
+    })()
+  }
+)
+
+export default usersRouter
