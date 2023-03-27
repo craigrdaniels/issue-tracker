05d1206231798ccf4b17f0b48803ceaf7eb339d8
Craig Daniels <craigrdaniels@gmail.com>
2023-03-27 20:44:33 +1000

    add bcrypt to user schema

--- backend/nodets/src/models/user.ts
+++ backend/nodets/src/models/user.ts
@@ -1,13 +1,27 @@
 import { Schema, model } from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
   _id: Schema.Types.ObjectId,
-  username: String,
-  password: String,
-  email: String,
+  username: { type: String, required: true, trim: true },
+  password: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
   role: { type: Schema.Types.ObjectId, refPath: 'Role' },
   profile_pic: String,
-  projects: { type: [Schema.Types.ObjectId], refPath: 'Project' }
+  projects: { type: [Schema.Types.ObjectId], refPath: 'Project' },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 })
+
+userSchema.methods.comparePassword = (password: string): boolean =>
+  bcrypt.compareSync(password, this.password)
 
 export default model('User', userSchema, 'users')
