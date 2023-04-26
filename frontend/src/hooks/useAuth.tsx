1a001b4acc59324fd5c281a11ef44d2063f3e955
Craig Daniels <craigrdaniels@gmail.com>
2023-04-26 21:05:19 +1000

    allow CORS, send credentials in request

---
+++ frontend/src/hooks/useAuth.tsx
@@ -0,0 +1,74 @@
+import { ReactNode, createContext, useContext, useMemo } from 'react'
+import useLocalStorage from './useLocalStorage'
+
+interface AuthContextType {
+  user: any
+  logIn: (
+    email: string,
+    password: string,
+    callback: VoidFunction
+  ) => Promise<void>
+  logOut: (callback: VoidFunction) => void
+}
+
+const AuthContext = createContext<AuthContextType>({} as AuthContextType)
+
+const AuthProvider = ({ children }: { children: ReactNode }): JSX.Element => {
+  const [user, setUser] = useLocalStorage('user', null)
+
+  const logIn = async (
+    email: string,
+    password: string,
+    callback: VoidFunction
+  ): Promise<void> => {
+    const location = window.location.hostname
+    const settings: RequestInit = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }
+
+    console.log('Logging on..')
+
+    const response = await fetch(
+      `http://${location}:3000/users/login`,
+      settings
+    )
+    const data = await response.json()
+    console.log(data.user)
+    setUser(data.user)
+
+    // navigate('/issues')
+    callback()
+  }
+
+  const logOut = (callback: VoidFunction) => {
+    setUser(null)
+    // navigate('/', { replace: true })
+    callback()
+  }
+
+  const values = useMemo(
+    () => ({
+      user,
+      logIn,
+      logOut,
+    }),
+    [user]
+  )
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+}
+
+const useAuth = (): AuthContextType => {
+  return useContext(AuthContext)
+}
+
+export { AuthProvider, useAuth }
--- frontend/src/pages/issues.tsx
+++ frontend/src/pages/issues.tsx
@@ -1,6 +1,23 @@
-import type { ReactElement } from 'react'
+import { ReactElement, useEffect } from 'react'
+import { useState } from 'react'
 
 const Issues = (): ReactElement => {
+  const [issues, setIssues] = useState<string[]>([])
+
+  useEffect(() => {
+    const location = window.location.hostname
+
+    const fetchData = async () => {
+      const response = await fetch(`http://${location}:3000/issues`, {
+        credentials: 'include',
+      })
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchData().catch(console.error)
+  }, [])
+
   return <div>Issues</div>
 }
 
