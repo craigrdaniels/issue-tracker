0e65f8d5394f9207debadaab81fb94536f677d4d
Craig Daniels <craigrdaniels@gmail.com>
2023-05-11 12:22:20 +1000

    add aggregate pipeline for individual issue

--- backend/nodets/src/controllers/IssueController.ts
+++ backend/nodets/src/controllers/IssueController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from 'express'
 import type { PipelineStage } from 'mongoose'
+import mongoose from 'mongoose'
 import Issue from '../models/issueModel.js'
 import type { IIssue } from '../models/issueModel.js'
 import User from '../models/userModel.js'
@@ -11,7 +12,7 @@ class IssueController {
     res: Response,
     next: NextFunction
   ): Promise<void> => {
-    const pipeline = [
+    const pipeline: PipelineStage[] = [
       {
         $lookup: {
           from: 'messages',
@@ -87,8 +88,119 @@ class IssueController {
     res: Response,
     next: NextFunction
   ): Promise<void> => {
-    const issue: IIssue | null = await Issue.findById(req.params.id)
-    res.status(200).json(issue)
+    const pipeline: PipelineStage[] = [
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(req.params.id)
+        }
+      },
+      {
+        $limit: 1
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'created_by',
+          foreignField: '_id',
+          as: 'created_by'
+        }
+      },
+      {
+        $set: {
+          created_by: {
+            $first: '$created_by'
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: 'projects',
+          localField: 'project',
+          foreignField: '_id',
+          as: 'project'
+        }
+      },
+      {
+        $set: {
+          project: {
+            $first: '$project'
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'assigned_users',
+          foreignField: '_id',
+          as: 'assigned_users'
+        }
+      },
+      {
+        $lookup: {
+          from: 'messages',
+          let: {
+            issue_id: '$_id'
+          },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ['$issue', '$$issue_id']
+                }
+              }
+            },
+            {
+              $lookup: {
+                from: 'users',
+                localField: 'created_by',
+                foreignField: '_id',
+                as: 'created_by'
+              }
+            },
+            {
+              $set: {
+                created_by: {
+                  $first: '$created_by'
+                }
+              }
+            },
+            {
+              $sort: {
+                created_at: 1
+              }
+            },
+            {
+              $project: {
+                _id: 1,
+                content: 1,
+                created_at: 1,
+                'created_by._id': 1,
+                'created_by.username': 1,
+                last_edit: 1
+              }
+            }
+          ],
+          as: 'messages'
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          'created_by._id': 1,
+          'created_by.username': 1,
+          'project._id': 1,
+          'project.name': 1,
+          'assigned_users._id': 1,
+          'assigned_users.username': 1,
+          messages: 1
+        }
+      }
+    ]
+
+    const issue: IIssue[] = await Issue.aggregate(pipeline)
+
+    res.status(200).json(issue[0])
   }
 
   static newIssue = async (
