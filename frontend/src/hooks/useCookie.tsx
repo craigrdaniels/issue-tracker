6e4596b58fb6d20c5980ea4bfba6aeb6f2d22b69
craigrdaniels <craigrdaniels@gmail.com>
2023-05-17 15:20:02 +1000

    create new useCookie hook

---
+++ frontend/src/hooks/useCookie.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+interface cookieOptions {
+  days?: number
+  path?: string
+  domain?: string
+  SameSite?: 'none' | 'Lax' | 'Strict'
+  Secure?: boolean
+  HttpOnly?: boolean
+}
+
+interface updateItem {
+  (value: string, options?: cookieOptions): void
+}
+
+const setCookie = (
+  key: string,
+  value: string,
+  options?: cookieOptions
+): void => {
+  const optionsWithDefaults = {
+    days: 7,
+    path: '/',
+    ...options,
+  }
+
+  const expires = new Date(
+    Date.now() + optionsWithDefaults.days * 864e5
+  ).toUTCString()
+
+  document.cookie =
+    key + '=' + encodeURIComponent(value) + '; expires=' + expires
+  // + optionsWithDefaults
+}
+
+const getCookie = (key: string): string =>
+  document.cookie.split('; ').reduce((total, currentCookie) => {
+    const item = currentCookie.split('=')
+    const itemKey = item[0]
+    const itemValue = item[1]
+
+    return itemKey === key ? decodeURIComponent(itemValue) : total
+  }, '')
+
+const useCookie = (key: string): [string, updateItem] => {
+  const [item, setItem] = useState(() => getCookie(key))
+
+  const updateItem = (value: string, options?: cookieOptions) => {
+    setItem(value)
+    setCookie(key, value, options)
+  }
+
+  return [item, updateItem]
+}
+
+export default useCookie
