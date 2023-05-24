f6e7ff29fbbbb841f3503eff1f263c3388aee58c
craigrdaniels <craigrdaniels@gmail.com>
2023-05-24 15:27:03 +1000

    add test coverage

--- backend/nodets/jest.config.cjs
+++ backend/nodets/jest.config.cjs
@@ -5,21 +5,33 @@ const config = {
   verbose: true,
   preset: 'ts-jest/presets/js-with-ts-esm',
   testEnvironment: 'node',
-  
+
   extensionsToTreatAsEsm: ['.ts'],
 
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
 
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx,js,jsx}',
+    '**/*.ts',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/dist/**',
+    '!**/coverage/**'
+  ],
+
   moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',  
+    '^(\\.{1,2}/.*)\\.js$': '$1'
   },
   transform: {
-    '^.+\\.(t|j)s$': ['ts-jest',
+    '^.+\\.(t|j)s$': [
+      'ts-jest',
       {
-        useESM: true,
+        useESM: true
       }
     ]
   }
-
 }
 module.exports = config
